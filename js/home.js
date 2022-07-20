e8ca8da1ef88144ccef767ec6cc2cdde63aa8615
Manuela Castro <manucastroa7@gmail.com>
2022-07-19 23:51:47 -0300

    subo funciones

---
+++ js/home.js
@@ -0,0 +1,51 @@
+function decimeNombre (){ 
+
+    let nombreUsuario = prompt("Insertar nombre")
+
+    let resultado = 'Hola ' + nombreUsuario
+
+    return resultado
+
+    
+}
+
+console.log(decimeNombre())
+
+
+
+function cantidad () {
+
+    adultos = parseInt (prompt ("Ingresar cantidad Adultos"))
+
+    chd= parseInt(prompt ("Ingresar cantidad CHD"))
+
+    let total = adultos + chd
+    
+    return total
+
+ 
+}
+
+console.log(cantidad())
+
+
+
+
+
+
+
+
+
+/* 
+
+let pasajerosAdultos = prompt("Cuantos adultos son? (valor por adulto 1000$")
+
+let pasajerosChd = prompt ("Cuantos pasajeros CHD son? (valor por chd 500$")
+
+let cantidadAdultos = pasajerosAdultos
+
+let cantidadChd = pasajerosChd
+
+function (adultos, chd) {
+
+} */ 
